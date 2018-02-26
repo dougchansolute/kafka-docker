aca525692d552bd7762d715d30513cc61005f30d
Douglas Chan <chan.doug@solute.us>
2018-02-26 15:17:11 -0800

    debug

--- start-kafka.sh
+++ start-kafka.sh
@@ -51,6 +51,7 @@ if [[ -n "$KAFKA_LISTENER_SECURITY_PROTOCOL_MAP" ]]; then
     echo "in creating add listener"
     echo "kafka protocol name is $KAFKA_PROTOCOL_NAME"
     echo "kafka host name is $KAFKA_HOST_NAME"
+    echo "kafka port is $KAFKA_PORT"
     echo "so add listener is $ADD_LISTENER"
   fi
 
